ca7e2ba3ea6672811ae658ca0c2aafa1b31fbf2f
bellatoris <bellatoris@snu.ac.kr>
2019-02-23 23:45:42 +0900

    more simplify

--- src/main/kotlin/com/doogie/datasourcecycle/DatasourceCycleApplication.kt
+++ src/main/kotlin/com/doogie/datasourcecycle/DatasourceCycleApplication.kt
@@ -6,25 +6,13 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Primary
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-import org.springframework.orm.jpa.JpaTransactionManager
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
-import org.springframework.transaction.PlatformTransactionManager
 import java.util.Properties
 import javax.sql.DataSource
 
 @SpringBootApplication
-@EnableJpaRepositories(
-    transactionManagerRef = "transactionManager",
-    entityManagerFactoryRef = "entityManagerFactory"
-)
 class DatasourceCycleApplication {
-    @Bean
-    fun transactionManager(): PlatformTransactionManager {
-        return JpaTransactionManager(entityManagerFactory().`object`!!)
-    }
-
     @Bean
     @Primary
     fun dataSourceProperties(): DataSourceProperties {
@@ -46,11 +34,11 @@ class DatasourceCycleApplication {
 
     @Bean
     fun entityManagerFactory(): LocalContainerEntityManagerFactoryBean {
-        val factory = LocalContainerEntityManagerFactoryBean()
-        factory.jpaVendorAdapter = HibernateJpaVendorAdapter()
-        factory.dataSource = dataSource()
-        factory.setPackagesToScan("com.doogie.datasourcecycle")
-        return factory
+        return LocalContainerEntityManagerFactoryBean().apply {
+            jpaVendorAdapter = HibernateJpaVendorAdapter()
+            dataSource = dataSource()
+            setPackagesToScan("com.doogie.datasourcecycle")
+        }
     }
 }
 
