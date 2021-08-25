73ce7a5cfd1a5acd195733c7297722fd090d9dce
bellatoris <bellatoris@snu.ac.kr>
2021-08-25 16:45:07 +0900

    update

--- src/main/kotlin/com/doogie/datasourcecycle/DatasourceCycleApplication.kt
+++ src/main/kotlin/com/doogie/datasourcecycle/DatasourceCycleApplication.kt
@@ -1,51 +1,69 @@
 package com.doogie.datasourcecycle
 
-import com.zaxxer.hikari.HikariDataSource
+import org.reactivestreams.Publisher
+import org.reactivestreams.Subscription
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Primary
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
-import java.util.Properties
-import javax.sql.DataSource
+import reactor.core.publisher.BaseSubscriber
+import reactor.core.publisher.Flux
+import reactor.core.scheduler.Schedulers
+import reactor.retry.Retry
+import java.time.Duration
+
 
 @SpringBootApplication
 class DatasourceCycleApplication {
     @Bean
-    @Primary
-    fun dataSourceProperties(): DataSourceProperties {
-        return DataSourceProperties()
+    fun runApplicion(): ApplicationRunner {
+        return ApplicationRunner {
+            val a = Retry
+                .anyOf<Unit>(RuntimeException::class.java)
+                .retryMax(5)
+                .timeout(Duration.ofHours(1)).doOnRetry { println("h") }
+
+            val b = object : Retry<Unit> by a {
+                override fun apply(t: Flux<Throwable>?): Publisher<Long> {
+                    return Flux.from(a.apply(t)).doOnError {
+                        println("hello")
+                    }
+                }
+            }
+
+            val hi = Flux.fromIterable(listOf(1, 2, 3, 4, 5))
+                .concatMap {
+                    if (it > 3) throw RuntimeException()
+                    Flux.fromIterable(listOf(1, 2, 3, 4, 5))
+                }
+                .retryWhen(b)
+                .limitRate(1)
+                .limitRequest(10)
+                .subscribeOn(Schedulers.elastic())
+
+//            hi.subscribe({ println(it) }, { e -> println(e) }, { println("done") }, { sub -> sub.request(Long.MAX_VALUE) })
+//            hi.subscribe(SimpleSubscriber)
+
+        }
     }
 
-    @Bean
-    fun slaveDataSource(): DataSource {
-        return dataSourceProperties()
-            .initializeDataSourceBuilder()
-            .type(HikariDataSource::class.java)
-            .build()
-    }
+    object SimpleSubscriber : BaseSubscriber<Int>() {
+        override fun hookOnSubscribe(subscription: Subscription) {
+            println("Subscribed")
+            request(1)
+        }
 
-    @Bean
-    fun dataSource(): DataSource {
-        return slaveDataSource()
-    }
+        override fun hookOnNext(value: Int) {
+            System.out.println(value)
+            request(1)
+        }
 
-    @Bean
-    fun entityManagerFactory(): LocalContainerEntityManagerFactoryBean {
-        return LocalContainerEntityManagerFactoryBean().apply {
-            jpaVendorAdapter = HibernateJpaVendorAdapter()
-            dataSource = dataSource()
-            setPackagesToScan("com.doogie.datasourcecycle")
+        override fun hookOnError(throwable: Throwable) {
+            println("haha")
         }
     }
 }
 
 fun main(args: Array<String>) {
-    runApplication<DatasourceCycleApplication>(*args) {
-       this.setDefaultProperties(Properties().also {
-           it["management.server.port"] = 18080
-       })
-    }
+    runApplication<DatasourceCycleApplication>(*args)
 }
